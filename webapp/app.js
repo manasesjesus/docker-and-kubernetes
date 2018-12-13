a04ac7529a7d3f84a4ea39183616f903d9dcbefa
Manasés Jesús Galindo Bello <contact@manasesjesus.com>
2018-12-13 10:46:11 +0100

    formatting code

--- webapp/app.js
+++ webapp/app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 const app = express();
 
 app.get("/", (req, res) => {
@@ -8,6 +7,4 @@ app.get("/", (req, res) => {
 
 app.listen(8080, () => {
     console.log("Listening on port 8080");
-    
 });
-
